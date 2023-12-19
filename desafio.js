37f58acb0898a06f3517479f6b1fbfe84b5f5a02
lanalima123 <alcylima03@gmail.com>
2023-12-19 15:47:08 -0300

    meu primeiro code

---
+++ desafio.js
@@ -0,0 +1,96 @@
+let herois = [["Maria", 11000],
+["pedro", 8000],
+["andreia", 9000]]
+
+//Maria nivel xp//
+if(herois[0][1]<= 1000){
+  
+ console.log("Parabéns " + herois[0][0]+" voçe está no nivel ferro") }
+ 
+ else if(2000>= herois[0][1] && herois[0][1]>= 1001){
+  
+    console.log("Parabéns " + herois[0][0]+" voçe está no nivel Bronze") }
+    
+    
+   else if(5000>= herois[0][1] && herois[0][1]>= 2001){
+  
+        console.log("Parabéns " + herois[0][0]+" voçe está no nivel prata") }
+
+        
+        else if(8000>= herois[0][1] && herois[0][1]>= 7001){
+  
+            console.log("Parabéns " + herois[0][0]+" voçe está no nivel platina") }
+
+else if(9000>= herois[0][1] && herois[0][1]>= 8001){
+  
+ console.log("Parabéns " + herois[0][0]+" voçe está no nivel ascendente") }
+
+ else if(10000>= herois[0][1] && herois[0][1]>= 9001){
+  
+console.log("Parabéns " + herois[0][0]+" voçe está no nivel imortal") }
+
+else if(10001<= herois[0][1]){
+  
+    console.log("Parabéns " + herois[0][0]+" voçe está no nivel radiante") }
+
+    //pedro nivel xp
+    if(herois[1][1]<= 1000){
+  
+        console.log("Parabéns " + herois[1][0]+" voçe está no nivel ferro") }
+        
+        else if(2000>= herois[1][1] && herois[1][1]>= 1001){
+         
+           console.log("Parabéns " + herois[1][0]+" voçe está no nivel Bronze") }
+           
+           
+          else if(5000>= herois[1][1] && herois[1][1]>= 2001){
+         
+               console.log("Parabéns " + herois[1][0]+" voçe está no nivel prata") }
+       
+               
+               else if(8000>= herois[1][1] && herois[1][1]>= 7001){
+         
+                   console.log("Parabéns " + herois[1][0]+" voçe está no nivel platina") }
+       
+       else if(9000>= herois[1][1] && herois[1][1]>= 8001){
+         
+        console.log("Parabéns " + herois[1][0]+" voçe está no nivel ascendente") }
+       
+        else if(10000>= herois[1][1] && herois[1][1]>= 9001){
+         
+       console.log("Parabéns " + herois[1][0]+" voçe está no nivel imortal") }
+       
+       else if(10001<= herois[1][1]){
+         
+           console.log("Parabéns " + herois[1][0]+" voçe está no nivel radiante") }
+
+           //nivel andreia
+
+  if(herois[2][1]<= 1000){
+
+    console.log("Parabéns " + herois[2][0]+" voçe está no nivel ferro") }
+            
+else if(2000>= herois[2][1] && herois[2][1]>= 1001){
+             
+console.log("Parabéns " + herois[2][0]+" voçe está no nivel Bronze") }
+               
+               
+else if(5000>= herois[2][1] && herois[2][1]>= 2001){
+             
+  console.log("Parabéns " + herois[2][0]+" voçe está no nivel prata") }
+           
+                  
+     else if(8000>= herois[2][1] && herois[2][1]>= 7001){
+              console.log("Parabéns " + herois[2][0]+" voçe está no nivel platina") }
+           
+         else if(9000>= herois[2][1] && herois[2][1]>= 8001){
+             
+            console.log("Parabéns " + herois[2][0]+" voçe está no nivel ascendente") }
+           
+            else if(10000>= herois[2][1] && herois[2][1]>= 9001){
+             
+           console.log("Parabéns " + herois[2][0]+" voçe está no nivel imortal") }
+           
+           else if(10001<= herois[2][1]){
+             
+               console.log("Parabéns " + herois[2][0]+" voçe está no nivel radiante") }
